c0c592080fd94ea3a241f1d4ed178ac98f546e2f
PatriciaHdz <32460268+patyhc4oct@users.noreply.github.com>
2020-09-11 09:50:56 -0600

    Cuenta Regresiva

---
+++ countdown/carousel/master.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  console.log("llamando");
+  // $('#carouselExampleIndicators').carousel()
+  $('.carousel').carousel()
+  // $('#myCarousel').carousel({
+  // pause: 'none'
+  // })
+});
